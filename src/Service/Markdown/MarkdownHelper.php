2265fb6f4d2547312989a48d041053e12056e97f
Renato Augusto <renatoaugusto.ads@gmail.com>
2022-05-27 10:04:16 -0300

    Criando um novo canal do monolog e injetando e configurando ele em nosso serviço MarkdownHelper

--- src/Service/Markdown/MarkdownHelper.php
+++ src/Service/Markdown/MarkdownHelper.php
@@ -3,26 +3,34 @@
 namespace App\Service\Markdown;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private AdapterInterface $cache;
     private MarkdownInterface $markdown;
+    private LoggerInterface $logger;
 
     public function __construct(
         AdapterInterface $cache,
-        MarkdownInterface $markdown
+        MarkdownInterface $markdown,
+        LoggerInterface $logger
     ) {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
-    public function parse(string $articleContent)
+    public function parse(string $source)
     {
-        $cacheItem = $this->cache->getItem('markdown_' . md5($articleContent));
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking about bacon agains!');
+        }
+
+        $cacheItem = $this->cache->getItem('markdown_' . md5($source));
         if (!$cacheItem->isHit()){
-            $cacheItem->set($this->markdown->transform($articleContent));
+            $cacheItem->set($this->markdown->transform($source));
             $this->cache->save($cacheItem);
         }
         return $cacheItem->get();
