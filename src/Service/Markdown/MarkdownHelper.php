30000ca3fbb16ae2cb9dfdc6349defeb648b7f88
Renato Augusto <renatoaugusto.ads@gmail.com>
2022-05-29 19:25:52 -0300

    Entendendo services.yaml e parametros no symfony, e brincando com o cache em dev e prod através do services.yaml ao invés de usar os ambientes do symfony

--- src/Service/Markdown/MarkdownHelper.php
+++ src/Service/Markdown/MarkdownHelper.php
@@ -11,21 +11,28 @@ class MarkdownHelper
     private AdapterInterface $cache;
     private MarkdownInterface $markdown;
     private LoggerInterface $logger;
+    private bool $isDebug;
 
     public function __construct(
         AdapterInterface $cache,
         MarkdownInterface $markdown,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        bool $isDebug
     ) {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $logger;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source)
     {
         if (stripos($source, 'bacon') !== false) {
             $this->logger->info('They are talking about bacon agains!');
+        }
+
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
         }
 
         $cacheItem = $this->cache->getItem('markdown_' . md5($source));
